cdf8a3ded702397aa8003f9421f3c020b832e246
Denys Poliakov <dev.dpolyakov@gmail.com>
2016-06-18 22:11:11 +0300

    Add project files.

---
+++ ScrofGallery.Domain/GalleryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace ScrofGallery.Domain
+{
+    public class GalleryItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public double Price { get; set; }
+    }
+}
